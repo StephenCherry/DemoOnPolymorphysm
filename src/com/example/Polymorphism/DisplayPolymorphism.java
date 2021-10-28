eca73bd4d02f22c37678b7126cea8ca7616b5eb2
cherry <olujareayodeji7@gmail.com>
2021-10-28 16:26:49 +0100

    Implementing Polymorphism

--- src/com/example/Polymorphism/DisplayPolymorphism.java
+++ src/com/example/Polymorphism/DisplayPolymorphism.java
@@ -1,5 +1,5 @@
 package com.example.Polymorphism;
-
+//Implementing Polymorphism in Java. Check out the snippets bellow.
 public class DisplayPolymorphism {
     public static void main(String[] args) {
         AnimalSounds animalSounds = new AnimalSounds();
